c048d4c517855d8a85d8caa8277378cc769faa9d
Lee Connelly <lee@leeconnelly.dev>
2022-07-17 04:14:41 +0100

    Added colors to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './main.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          300: 'hsl(212, 45%, 89%)',
+          500: 'hsl(220, 15%, 55%)',
+          800: 'hsl(218, 44%, 22%)',
+        },
+      },
+    },
   },
   plugins: [],
 }
